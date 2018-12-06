c2aa5673b58e96ff5025b9f4d5b0595c1d348b81
Ben Hart <benjmhart@gmail.com>
2018-12-06 16:30:13 -0500

    day 6 - working

--- day6-2.hs
+++ day6-2.hs
@@ -4,17 +4,11 @@ import qualified Data.Set as S
 import qualified Data.Map as M
 import Data.Monoid(Sum(..), getSum)
 import Control.Applicative
-import Debug.Trace
-
-trace' x = traceShow x x 
 
 main :: IO()
 main = do
   input <- readFile $ "./inputs/day6-1.txt"
-  let origins = parse input
-  let areaList = maxArea origins
-  let isWithinRange' = isWithinRange maxDist origins
-  putStr $ (++ "\n"). show . length . filter isWithinRange' $ areaList
+  putStr $ (++ "\n"). show . solve $ input
   return ()
 
 type Coord = (Int, Int)
@@ -22,27 +16,28 @@ type Coord = (Int, Int)
 maxDist :: Int
 maxDist = 10000
 
+solve :: String -> Int
+solve xs = length [ (x,y) 
+               | x <- [maxL..maxR] 
+               , y <- [maxT..maxB ]
+               ,   (< maxDist) . sum . map (getMHDistance (x,y)) $ origins 
+               ]
+  where origins = parse xs
+        maxL = minimum . map fst $ origins
+        maxT = minimum . map snd $ origins
+        maxR = maximum . map fst $ origins
+        maxB = maximum . map snd $ origins
+        
+
 parse :: String -> [Coord]
-parse xs = piped
+parse xs = origins
  where
-  piped = map pipeline . lines $ xs
+  origins = map pipeline . lines $ xs
   pipeline = toPair . map toInt . words . isNotPunctuation 
   isNotPunctuation = filter (not . isPunctuation)
   toInt x = read x :: Int
   toPair (a:b:[]) = (a, b)
 
-maxArea :: [Coord] -> [Coord]
-maxArea origins = liftA2 (,) [maxL..maxR] [maxT..maxB]
-  where
-    maxR = (+ maxDist) . minimum . map fst $ origins
-    maxB = (+ maxDist) . minimum . map snd $ origins
-    maxL = minus maxDist . maximum . map fst $ origins
-    maxT = minus maxDist . maximum . map snd $ origins
-    minus x y = y - x
-
-isWithinRange :: Int -> [Coord] -> Coord -> Bool
-isWithinRange distance origins coord = 
- (< distance) . getSum $ foldMap (Sum . getMHDistance coord) $ origins
         
 
 getMHDistance :: Coord -> Coord -> Int
